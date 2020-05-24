d514009a2faeb1c821711267a35eeac362485c4f
ComsaAna <62028016+ComsaAna@users.noreply.github.com>
2020-05-24 15:57:28 +0300

    Add files via upload

--- Ausnahmen.cpp
+++ Ausnahmen.cpp
@@ -1,46 +1,81 @@
 #include <iostream>
+#include <exception>
 
 using namespace std;
 
+class myex {
+	/*Eigene Exception class, die ein Exception erstellt*/
+public:
+	myex() {
+
+	}
+	int myexception() {
+		//gibt eine Zahl zuruck und erlaubt in dem catch weiterzugehen
+		return 20;
+	}
+};
+
 class Exceptions {
+	/*Mehrere Funktionen, die die falschen Eingabedaten verbessert
+	zB. ein string in einer int Variable zu lesen*/
+public:
+	myex except;
 public:
 	Exceptions() {
-
 	}
 
 	bool LikeExcept(int &likes) {
+		//Exception fur Likes
 		try {
+			//uberpruft ob die Anzahl der Likes ein Integer ist
 			cout << "Likes= ";
 			cin >> likes;
+			if (cin.fail())
+				//sonst wirft sie ein Exception
+				throw except.myexception();
 		}
 		catch(...) {
-			cout << "\nOh no...An exception occured. Please enter a valid number of Likes\n";
+			//sie kommt hier durch die geworfene Exception an und schreibt das auf dem Bildschirm an
+			cout << "Oh no...An exception occured. Please enter a valid number of Likes\n";
 			return false;
 		}
 		return true;
 	}
 
 	bool YearExcept(int &year) {
+		//Exception fur das Jahr
 		try {
+			//uberpruft ob das Jahr ein Integer ist
 			cout << "Year= ";
 			cin >> year;
+			if (cin.fail())
+				//sonst wirft sie ein Exception
+				throw except.myexception();
 		}
 		catch (...) {
-			cout << "\nOh no...An exception occured. Please enter a valid year\n";
+			//sie kommt hier durch die geworfene Exception an und schreibt das auf dem Bildschirm an
+			cout << "Oh no...An exception occured. Please enter a valid year\n";
 			return false;
 		}
 		return true;
 	}
 
 	bool OptionExcept(int &option) {
+		//Exception fur die Option, die man eingibt
 		try {
+			//uberpruft ob die Option ein Integer ist
 			cout << "Your option= ";
 			cin >> option;
+			if(cin.fail())
+				//sonst wirft sie ein Exception
+				throw except.myexception();
 		}
-		catch (...) {
-			cout << "\nOh no...An exception occured. Please type a valid option\n";
+		catch (int e) {
+			//sie kommt hier durch die geworfene Exception an und schreibt das auf dem Bildschirm an
+			cout << "Oh no...An exception occured. Please type a valid option\n";
 			return false;
 		}
 		return true;
 	}
 };
+
